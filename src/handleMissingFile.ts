c9dabbb4f05c499f26b0c95a7d46fc1363108357
Abhishek Yelley <yabhishek.19.cse@anits.edu.in>
2024-03-22 00:45:00 +0530

    upd: added finish event listener on write stream

--- src/handleMissingFile.ts
+++ src/handleMissingFile.ts
@@ -2,47 +2,59 @@ import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
 import fs from 'fs';
 
-type WriteStream = {
-    stream?: fs.WriteStream
-}
+function writeToNewFile(LYRIC_FILE_NAME: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+        const writeStream: { stream?: fs.WriteStream } = {};
 
-async function writeToNewFile(LYRIC_FILE_NAME: string) {
-    const writeStream: WriteStream = {};
-    try{
         writeStream.stream = fs.createWriteStream(`./lyrics/${LYRIC_FILE_NAME}`);
-    } catch (e) {
-        console.error("COULD NOT CREATE FILE!");
-        console.log(`PROCESS EXITING!`);
-        process.exit();
-    }
-    writeStream.stream.write(`# *${LYRIC_FILE_NAME.slice(0, -3).toUpperCase()}*\r\n`);
-    writeStream.stream.write(`> *Write your lyrics here*`);
-    writeStream.stream.end();
+        writeStream.stream.write(`# *${LYRIC_FILE_NAME.slice(0, -3).toUpperCase()}*\r\n`);
+        writeStream.stream.write(`> *Write your lyrics here*`);
+        writeStream.stream.end();
+
+        writeStream.stream.on('finish', () => {
+            console.log("FILE CREATED!");
+            resolve();
+        });
+        writeStream.stream.on('error', (err) => {
+            console.error("COULD NOT CREATE FILE!");
+            reject(err);
+        });
+    })
 }
-
-
-
+/**
+ * Asks user if a new file is to be created.
+ * 
+ * If yes, the file is created.
+ * 
+ * Otherwise process is terminated.
+ */
 export default async function handleMissingFile(ARGS_INCLUDE_Y: boolean, LYRIC_FILE_NAME: string) {
-    if ( !fs.existsSync(`./lyrics/${LYRIC_FILE_NAME}`) ) {
+    if (fs.existsSync(`./lyrics/${LYRIC_FILE_NAME}`))
+        return;
+
+    const rl = readline.createInterface({
+        input: input as NodeJS.ReadableStream,
+        output: output as NodeJS.WritableStream
+    });
+
+    try {
         if (ARGS_INCLUDE_Y) {
-            writeToNewFile(LYRIC_FILE_NAME);
+            await writeToNewFile(LYRIC_FILE_NAME);
             return;
         }
-
-        const rl = readline.createInterface({
-            input: input as NodeJS.ReadableStream,
-            output: output as NodeJS.WritableStream
-        });
 
         const makeNewFile = await rl.question(`Given file: ${LYRIC_FILE_NAME} doesn't exist. Would you like to create one (Y/N)?`);
 
         if (makeNewFile === 'y' || makeNewFile === 'Y') {
-            writeToNewFile(LYRIC_FILE_NAME);
+            await writeToNewFile(LYRIC_FILE_NAME);
             rl.close();
             return;
         }
-        console.log(`PROCESS EXITING!`);
+    } catch (e) {
+        console.error(e);
+        console.log(`EXITING PROCESS!`);
         rl.close();
         process.exit();
     }
+
 }
